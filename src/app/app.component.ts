e30f87e16ab540cd44f6ef4100ad167af071f2e3
spsiliras <104709495+spsiliras@users.noreply.github.com>
2024-07-07 13:44:38 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { HeaderComponent } from './header/header.component';
-import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
-import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
+  standalone: false,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent, UserComponent, TasksComponent],
 })
 export class AppComponent {
   title = 'first-angular-app';
--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,15 +1,12 @@
 import { Component, Input, inject } from '@angular/core';
 import { type Task } from './task.model';
-import { CardComponent } from '../../shared/card/card.component';
-import { DatePipe } from '@angular/common';
 import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-task',
-  standalone: true,
+  standalone: false,
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
-  imports: [CardComponent, DatePipe],
 })
 export class TaskComponent {
   @Input({ required: true }) task!: Task;
